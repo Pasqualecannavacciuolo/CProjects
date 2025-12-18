f290e784a7c185c7e076f8631b1a0970fb463d52
Pasqualecannavacciuolo <p.cannavacciuolo@icloud.com>
2025-12-18 23:08:07 +0100

    Simple Cipher

---
+++ Cesar Cipher/src/main.c
@@ -0,0 +1,32 @@
+#include <stdio.h>
+#include <stdlib.h>
+#include <string.h>
+
+// 0 1 2 3 4 5 6 7 8 9 10 11 12 13 14 15 16 17 18 19 20 21 22 23 24 25
+// A B C D E F G H I J K  L  M  N  O  P  Q  R  S  T  U  V  W  X  Y  Z
+
+int main(void)
+{
+    int shift = 4;
+    char *plain = malloc(strlen("Cesare") + 1);
+    strcpy(plain, "Cesare");
+
+    char *encrypted = malloc(strlen(plain));
+
+    int size = strlen(plain);
+    int i = 0;
+
+    while (i < size)
+    {
+        encrypted[i] = plain[i] + shift;
+        i++;
+    }
+    encrypted[size] = '\0';
+
+    printf("%s\n", encrypted);
+
+    free(plain);
+    free(encrypted);
+
+    return 0;
+}

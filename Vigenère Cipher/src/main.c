9b834d8e89fdf1039f051cc28eed6460a2f5a4e6
Pasqualecannavacciuolo <p.cannavacciuolo@icloud.com>
2025-12-18 23:52:03 +0100

    FIX: added support to command line arguments

--- Vigenère Cipher/src/main.c
+++ Vigenère Cipher/src/main.c
@@ -5,27 +5,31 @@
 // 0 1 2 3 4 5 6 7 8 9 10 11 12 13 14 15 16 17 18 19 20 21 22 23 24 25
 // A B C D E F G H I J K  L  M  N  O  P  Q  R  S  T  U  V  W  X  Y  Z
 
-int main(void)
+int main(int argc, char *argv[])
 {
     // char c = 'C';
     // int integer_value = (c - 'A') % 26; // Ogni lettera dell'alfabeto viene mappata ad un numero da 0 a 25
     // printf("%d\n", integer_value);
 
-    char *plain = malloc(strlen("RAPPORTOIMMEDIATO") + 1);
+    if (argc < 3 || argc > 3)
+    {
+        fprintf(stderr, "Uso: %s <plain_text> <worm_phrase>\n", argv[0]);
+        return 1;
+    }
+
+    char *plain = argv[1];
     if (!plain)
     {
         perror("Error allocating PLAIN");
         return 1;
     }
-    strcpy(plain, "RAPPORTOIMMEDIATO");
 
-    char *worm = malloc(strlen("VERMEVERMEVERMEVE") + 1);
+    char *worm = argv[2];
     if (!worm)
     {
         perror("Error allocating WORM");
         return 1;
     }
-    strcpy(worm, "VERMEVERMEVERMEVE");
 
     size_t i = 0;
     size_t size = strlen(plain);
